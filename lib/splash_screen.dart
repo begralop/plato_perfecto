7d4694aca992546fb0c15b9f801be5b7c6e696e3
Belén <bgralop@epsg.upv.es>
2023-12-15 14:05:35 +0100

    Splash screen and home screen working

---
+++ lib/splash_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
+import 'package:plato_perfecto/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (_) => const HomeScreen()
+        ));
+    });
+  }
+
+@override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.asset(
+                'assets/images/plato_perfecto_logo.png',
+                width: 200,
+                height: 200,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              width: 600,
+              height: 200,
+              child: Lottie.asset('assets/lottie/loading_animation.json'),
+            ),
+          ],
+    )
+    );
+  }
+}
